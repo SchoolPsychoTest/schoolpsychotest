83dbb1c894d173ae5437c6e459b3bce532a26b0a
aisse-258 <y-kate@ukr.net>
2021-03-21 17:05:18 +0300

    dia

--- front.js
+++ front.js
@@ -14,7 +14,32 @@ $('#get-result').click(function(){
         }
     }
     if(answers.length == 13){
-        document.getElementById('result').innerHTML='';
+        document.getElementById('result').innerHTML='<canvas id="marksChart" width="600" height="400"></canvas>';
         console.log(answers);
+		var diaData = {critical:0, creative:0, communicative:0, cooperative:0};
+		for(let i=1; i<13; i++){
+			let ans_obj = JSON.parse(answers[i].value);
+			diaData.critical += Number(ans_obj.critical);
+			diaData.creative += Number(ans_obj.creative);
+			diaData.communicative += Number(ans_obj.communicative);
+			diaData.cooperative += Number(ans_obj.cooperative);
+		}
+
+		console.log(diaData);
+        var marksCanvas = document.getElementById("marksChart");
+
+		var marksData = {
+		labels: ["Критическое мышление", "Креативное мышление", "Коммуникация", "Кооперация"],
+		datasets: [{
+			label: answers[0].value,
+			backgroundColor: "rgba(200,0,0,0.2)",
+			data: [diaData.critical, diaData.creative, diaData.communicative, diaData.cooperative]
+		}]
+		};
+
+		var radarChart = new Chart(marksCanvas, {
+		type: 'radar',
+		data: marksData
+		});
     }
 })
