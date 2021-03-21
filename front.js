a8d66dc7ed4e2941c9d946de52e4ab5595e1b325
aisse-258 <y-kate@ukr.net>
2021-03-21 17:44:14 +0300

    name input and dia options

--- front.js
+++ front.js
@@ -1,21 +1,22 @@
 $('#get-result').click(function(){
-    var answers = [];
-    for(let i=0;i<13;i++){
-        let question = document.getElementsByName('a'+i);
-        for (let ans of question) {
-            if (ans.checked){
-                answers.push(ans);
-                break;
-            }
-        }
-        if(!answers[i]){
-            document.getElementById('result').innerHTML='<p style="color: red">Даны не все ответы!</p>';
-            break;
-        }
-    }
-    if(answers.length == 13){
-        document.getElementById('result').innerHTML='<canvas id="marksChart" width="600" height="400"></canvas>';
-        console.log(answers);
+	var answers = [];
+	answers[0] = document.getElementsByName('a0')[0].value == "" ? "Вы" : document.getElementsByName('a0')[0].value;
+	for(let i=1;i<13;i++){
+		let question = document.getElementsByName('a'+i);
+		for (let ans of question) {
+			if (ans.checked){
+				answers.push(ans);
+				break;
+			}
+		}
+		if(!answers[i]){
+			document.getElementById('result').innerHTML='<p style="color: red">Даны не все ответы!</p>';
+			break;
+		}
+	}
+	if(answers.length == 13){
+		document.getElementById('result').innerHTML='<canvas id="marksChart" width="600" height="400"></canvas>';
+		console.log(answers);
 		var diaData = {critical:0, creative:0, communicative:0, cooperative:0};
 		for(let i=1; i<13; i++){
 			let ans_obj = JSON.parse(answers[i].value);
@@ -26,24 +27,36 @@ $('#get-result').click(function(){
 		}
 
 		console.log(diaData);
-        var marksCanvas = document.getElementById("marksChart");
+		var marksCanvas = document.getElementById("marksChart");
 
 		var marksData = {
 		labels: ["Критическое мышление", "Креативное мышление", "Коммуникация", "Кооперация"],
 		datasets: [{
-			label: answers[0].value,
+			label: answers[0],
 			backgroundColor: "rgba(200,0,0,0.2)",
 			data: [diaData.critical, diaData.creative, diaData.communicative, diaData.cooperative]
-		}, {
-			label: "0000",
-			backgroundColor: "rgba(0,0,200,0.2)",
-			data: [0, 0, 0, 0]
 		}]
 		};
-
+		var chartOptions = {
+			scale: {
+				ticks: {
+					beginAtZero: true,
+					min: 0,
+					max: 6,
+					stepSize: 1
+				},
+				pointLabels: {
+					fontSize: 18
+				}
+			},
+			legend: {
+				position: 'left'
+			}
+		};
 		var radarChart = new Chart(marksCanvas, {
 		type: 'radar',
-		data: marksData
+		data: marksData,
+		options: chartOptions
 		});
-    }
+	}
 })
